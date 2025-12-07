36a0b86569fe572a81338dd35da5ea401c1dfed9
xuanhua2318 <thuongxuan.2323@gmail.com>
2025-12-07 16:13:48 -0500

    first upload

---
+++ Hua_landscape.py
@@ -0,0 +1,10 @@
+def main():
+    account = input("P-Preferred, R-Regular, N-New ")
+    num_Bushes = int(input())
+    bed_width = int(input())
+    bed_length = int(input())
+    fertile_area = int(input())
+    
+    
+    
+main()
